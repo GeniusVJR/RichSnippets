d62533c340f07541b1890f7e0ae0fb05f1def137
geniusvjr <geniusvjr@gmail.com>
2016-09-25 20:37:58 +0800

    添加常量

--- snippet/src/main/java/com/francis/snippet/SnippetsText.java
+++ snippet/src/main/java/com/francis/snippet/SnippetsText.java
@@ -15,6 +15,14 @@ import android.widget.EditText;
  */
 public class SnippetsText extends EditText {
 
+	public static final int FORMAT_BOLD = 0x01;
+	public static final int FORMAT_ITALIC = 0x02;
+	public static final int FORMAT_UNDERLINED = 0x03;
+	public static final int FORMAT_STRIKETHROUGH = 0x04;
+	public static final int FORMAT_BULLET = 0x05;
+	public static final int FORMAT_QUOTE = 0x06;
+	public static final int FORMAT_LINK = 0x07;
+
 	public SnippetsText(Context context) {
 		super(context);
 	}
